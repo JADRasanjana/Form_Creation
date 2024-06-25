f8990e927c0bf8a0237bcd221d65a23ac81f8c06
Your Name <your_email@example.com>
2024-06-25 16:17:50 +0000

    create file structure for library system

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+import express from "express"
+//import booksRoutes from './src/routes/bookRoutes.js';
+import { notFound, errorHandler } from './src/middlewares/bookMiddlewares.js';
+import connectDB from "./src/dbConnect/connectDB.js";
+import { config } from 'dotenv';
+import mongoose from "mongoose";
+config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+// Middleware
+// app.use(bodyParser.json());
+app.use(express.json());
+// Routes
+//app.use('/api', booksRoutes);
+
+// 404 Not Found middleware
+app.use(notFound);
+
+// Global error handler
+app.use(errorHandler);
+
+const connection = mongoose.connection;
+connection.once("open",() => {
+    console.log("Your MongoDB connection is success!!!!!");
+})
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGODB_URL);
+        app.listen(PORT, () => {
+        console.log(`Server is listening on port ${PORT}...`);
+
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
+
+
+
---
+++ backend/src/middlewares/bookMiddlewares.js
@@ -0,0 +1,8 @@
+export const notFound = (req, res, next) => {
+    res.status(404).json({ message: 'Route does not exists' });
+};
+
+export const errorHandler = (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Internal server error' });
+};
